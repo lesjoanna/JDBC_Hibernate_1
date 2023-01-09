5a8a67155d257ba210a1c399d8f7a4067d149ec6
lesjoanna <118379312+lesjoanna@users.noreply.github.com>
2023-01-09 19:32:27 +0100

    Movie - part 1

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
+
+    private static final ArrayList<String> movies = new ArrayList<>();
+
     private boolean running = true;
 
     public void startMenu() {
@@ -34,9 +38,11 @@ public class Menu {
 
             case 1:
                 System.out.println("Dodawanie filmow");
+                addMovie();
                 break;
             case 2:
                 System.out.println("Wyświetlanie");
+                showMovies();
                 break;
             case 3:
                 System.out.println("Koniec");
@@ -45,5 +51,57 @@ public class Menu {
 
 
         }
+
+
     }
+    private static void addMovie() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter movie title: ");
+        String title = scanner.nextLine();
+
+        String releaseDate = "";
+        while (true) {
+            System.out.print("Enter movie release date (between 1800 and 2100): ");
+            releaseDate = scanner.nextLine();
+
+            // Validate release date
+            if (releaseDate.matches("[0-9]{4}") &&
+                    Integer.parseInt(releaseDate) >= 1800 &&
+                    Integer.parseInt(releaseDate) <= 2100) {
+                break;
+            } else {
+                System.out.println("Invalid release date. Please try again.");
+            }
+        }
+
+        System.out.print("Enter movie genre: ");
+        String genre = scanner.nextLine();
+
+        System.out.print("Enter movie rating: ");
+        String rating = scanner.nextLine();
+
+        // Create a movie object
+        Movie movie = new Movie(title, releaseDate, genre, rating);
+
+        System.out.println(movie);
+
+        // Add movie to list
+        movies.add(String.valueOf(movie)) ;
+        System.out.println("Movie added.");
+    }
+
+    private static void showMovies() {
+        if (movies.isEmpty()) {
+            System.out.println("There is no movies on the list.");
+        } else {
+            System.out.println("Movies: ");
+
+            for (String movie : movies) {
+                System.out.println(movie);
+            }
+        }
+    }
+
+
 }
